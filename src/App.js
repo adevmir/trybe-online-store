9dff6796b6d1823568d0bcb3007925df1ea9e144
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-26 16:36:50 -0300

    Correçao do lint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import getCategories from './services/api';
+// import getCategories from './services/api';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div>Hello</div>
   );
 }
 
