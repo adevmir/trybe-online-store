58988db9e0389fbaf921daeb4f6b6433a92ffb01
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-26 17:43:10 -0300

    Implementação do carrinho de compras e suas rotas necessarias

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import ShoppingCart from './Pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/shopping-cart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
