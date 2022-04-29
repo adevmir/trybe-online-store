25f624dda98d3c8d1d3622e5108a1bb04bd8bd3f
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-29 19:11:28 -0300

    WIP - Linkando Home à ShoppingCart

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import LinkShopCart from '../Components/LinkShopCart';
 
 class Home extends React.Component {
   render() {
     return (
       <main>
         <h2>Página Home</h2>
+        {/* Link para o carrinho de compras */}
+        <LinkShopCart />
+        {/* Mensagem inicial */}
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
