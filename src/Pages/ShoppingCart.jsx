2f75fbba8f6249b8e9350db02362206c1969d0bb
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-29 19:14:08 -0300

    WIP - Foi adicionada uma mensagem que sera exibida qnd o carrinho estiver vazio, também foi adicionada um link para retorar a Home

--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-class ShoppingCart extends React.Component {}
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <main>
+        <h2>Página do Carrinho de Compras</h2>
+        <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+        <Link to="/">
+          Continuar comprando
+        </Link>
+      </main>
+    );
+  }
+}
 
 export default ShoppingCart;
