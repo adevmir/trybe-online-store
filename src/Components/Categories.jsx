ee089fe2cdb56a04678bfd82aa15aa964f433eae
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-30 09:30:10 -0300

    WIP - Foi escrita a função que busca as categorias na API e lista na tela

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
-class Categories extends React.Component {}
+class Categories extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listCategories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const listCategories = await getCategories();
+    this.setState({
+      listCategories,
+    });
+  }
+
+  render() {
+    const { listCategories } = this.state;
+
+    return (
+      <aside>
+        <ul>
+          Categorias:
+          {
+            listCategories.map(({ id, name }) => (
+              <li key={ id }>
+                <button
+                  name="category"
+                  value={ id }
+                  type="button"
+                  data-testid="category"
+                >
+                  {name}
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </aside>
+    );
+  }
+}
 
 export default Categories;
