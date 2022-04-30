d9e025c4124cdf4727f723bb99aa700ca860cf49
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-30 13:45:35 -0300

    WIP - Foi usada a função getDetails que usa a API para obter informações de um produto especifico pelo seu id, o retorno dessa função foi usado para exibir detalhes do produto, nem todos os detalhes foram usados

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import LinkShopCart from '../Components/LinkShopCart';
+import { getDetails } from '../services/api';
 
-class ProductDetails extends React.Component {}
+class ProductDetails extends React.Component {
+  constructor() {
+    super();
+    this.state = ({
+      details: [],
+    });
+  }
+
+  // Os detalhes mais especificos do produto então em "attributes"
+  // Fotos em varios angulos então em "pictures"
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+
+    const details = await getDetails(id);
+    this.setState({
+      details,
+    });
+  }
+
+  render() {
+    const { details } = this.state;
+    const { id, title, price, pictures, thumbnail } = details;
+    console.log(Array.isArray(pictures));
+    console.log(pictures);
+    return (
+      <main>
+        <h2>Página de Detalhes</h2>
+        <div id={ id }>
+          <h2 data-testid="product-detail-name">{ title }</h2>
+          <img src={ thumbnail } alt={ title } />
+          <p>{`R$ ${price}`}</p>
+        </div>
+        <LinkShopCart />
+      </main>
+    );
+  }
+}
+
+ProductDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default ProductDetails;
