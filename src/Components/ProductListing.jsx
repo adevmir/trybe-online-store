a1fab188fccf9ee50c8b08fef2f216a6f7d122a4
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-30 11:56:38 -0300

    WIP - Foi adicionada as funcionalidade de ProductListing, ela ira renderizar apenas um item passado, deve-se usa-la em loop para renderizar multiplos produtos

--- src/Components/ProductListing.jsx
+++ src/Components/ProductListing.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class ProductListing extends React.Component {}
+class ProductListing extends React.Component {
+  render() {
+    const {
+      title,
+      price,
+      thumbnail,
+      id,
+    } = this.props;
+    return (
+      <div
+        id={ id }
+        data-testid="product"
+      >
+        {/* Nome do froduto */}
+        <h2>{ title }</h2>
+        {/* Foto do produto */}
+        <img src={ thumbnail } alt={ `Imagem de ${title}.` } />
+        {/* Preço do produto */}
+        <p>{`R$ ${price}`}</p>
+      </div>
+    );
+  }
+}
+
+ProductListing.propTypes = {
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default ProductListing;
