8f4185dc37d8cc43f3a7b68b128128d2f4d18b04
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-30 17:20:29 -0300

    WIP - Foi criado um estado onde ficaria armazenado todos os produtos comprados; foi criada uma função capaz de adicionar novos produtos a esse estado, a função tabem é capaz de aumentar a quantidade de um item já existente; essa função foi passada via props para o Home, onde seria usado para adicionar itens ao carrinho; o estado que é alterado pela função foi passado para ShoppingCart

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,7 +5,59 @@ import ShoppingCart from './Pages/ShoppingCart';
 import ProductDetails from './Pages/ProductDetails';
 
 class Main extends React.Component {
+  constructor() {
+    super();
+    this.addCart = this.addCart.bind(this);
+    this.state = ({
+      shoppingList: [],
+    });
+  }
+  /*
+    Formato de shoppingList:
+
+    shoppingList = [ obj1, obj2, ..., objn ]
+    obj = {
+      id: id do produto
+      quantity: quantos produtos foram adicionados
+    }
+  */
+
+  // Adiciona produto ao carrinho de compras
+  addCart(idProduct) {
+    const { shoppingList } = this.state;
+
+    // Verifica se o id já existe em shoppingList: true - já existe, false -ainda não existe
+    const idExists = shoppingList.some((product) => product.id === idProduct);
+
+    // Caso o item ainda não tenha sido adicionado
+    if (!idExists) {
+      const itemObj = {
+        id: idProduct,
+        quantity: 1,
+      };
+      this.setState((old) => ({
+        shoppingList: [...old.shoppingList, itemObj],
+      }));
+    } else {
+      // Caso o item já tenha sido adicionado
+      shoppingList.forEach((infoProduct, index) => {
+        if (infoProduct.id === idProduct) {
+          const amount = infoProduct.quantity + 1;
+          const reform = shoppingList;
+          reform[index] = {
+            id: infoProduct.id,
+            quantity: amount,
+          };
+          this.setState({
+            shoppingList: reform,
+          });
+        }
+      });
+    }
+  }
+
   render() {
+    const { shoppingList } = this.state;
     return (
       <>
         <header>
@@ -17,10 +69,24 @@ class Main extends React.Component {
         <BrowserRouter>
           <Switch>
             {/* Rota para Home */}
-            <Route exact path="/" component={ Home } />
+            <Route
+              exact
+              path="/"
+              render={ () => (
+                <Home
+                  addItemCart={ this.addCart }
+                />) }
+            />
 
             {/* Rota para o Carrinho de Compras */}
-            <Route path="/shopping-cart" component={ ShoppingCart } />
+            <Route
+              path="/shopping-cart"
+              // component={ ShoppingCart }
+              render={ () => (
+                <ShoppingCart
+                  listItems={ shoppingList }
+                />) }
+            />
 
             {/* Rota para Detalhes do produto */}
             <Route
